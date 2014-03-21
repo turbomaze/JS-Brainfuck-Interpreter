21f01ddd1eca46bb789747530aa34cf6dad9db53
Anthony Dee <bobhoward72@gmail.com>
2014-03-20 23:34:54 -0700

    Ports the java runProgram function to javascript and optimizes the execution loop.

--- js/brainfuck.js
+++ js/brainfuck.js
@@ -47,8 +47,105 @@ function initBFInterpreter() {
 	});
 }
 
+//program is a String, inputs is an array of integers in [0, 256)
 function runProgram(program, inputs) {
-	return 7;
+	var tapeLen = 128; //128 cells on the tape
+	var checkEvery = 2*1000; //checks the time every this many iterations to ensure it isn't taking too long
+	var instructions = program.replace(/ /g, '').split(''); //essentially String.toCharArray()
+	var ret = 0;
+
+	///////////////////////////////
+	//interpreter state variables//
+	var tape = [];
+	for (var ai = 0; ai < tapeLen; ai++) tape.push(0);
+	var ins_ptr = 0;
+	var dat_ptr = 0;
+	var inp_ptr = 0;
+
+	///////////////
+	//interpreter//
+	var s = currentTimeMillis();
+	while (currentTimeMillis() - s < tapeLen/2) { //timeout
+		for (var ai = 0; ai < checkEvery; ai++) {
+			if (ins_ptr >= instructions.length) {
+				return ret; //finished reading all instructions
+			}
+
+			switch (instructions[ins_ptr]) {
+				case '>':
+					dat_ptr = (dat_ptr+1)%tapeLen; //loops around
+					ins_ptr += 1;
+					break;
+				case '<':
+					dat_ptr = dat_ptr-1 < 0 ? tapeLen-1 : dat_ptr-1; //loops around
+					ins_ptr += 1;
+					break;
+				case '+':
+					tape[dat_ptr] = (tape[dat_ptr]+1)%256; //wrap around
+					ins_ptr += 1;
+					break;
+				case '-':
+					tape[dat_ptr] = tape[dat_ptr]-1 < 0 ? 255 : tape[dat_ptr]-1; //wrap around
+					ins_ptr += 1;
+					break;
+				case '.':
+					ret += tape[dat_ptr];
+					ins_ptr += 1;
+					break;
+				case ',':
+					if (inp_ptr < inputs.length) {
+						tape[dat_ptr] = inputs[inp_ptr];
+						inp_ptr += 1;
+					} else {
+						tape[dat_ptr] = 0; //clear it if there aren't any inputs to grab from
+					}
+					ins_ptr += 1;
+					break;
+				case '[':
+					if (tape[dat_ptr] > 0) {
+						ins_ptr += 1;
+					} else {
+						var rel_indent = 0;
+						while (true) {
+							ins_ptr += 1;
+							if (ins_ptr >= instructions.length) return ''; //no match
+							
+							if (instructions[ins_ptr] == '[') {
+								rel_indent += 1;
+							} else if (instructions[ins_ptr] == ']' && rel_indent != 0) {
+								rel_indent -= 1;
+							} else if (instructions[ins_ptr] == ']' && rel_indent == 0) {
+								ins_ptr += 1; //found match, go to next command and move on
+								break;
+							}
+						}
+					}
+					break;
+				case ']':
+					if (tape[dat_ptr] > 0) {
+						var rel_indent = 0;
+						while (true) {
+							ins_ptr -= 1;
+							if (ins_ptr < 0) return ''; //no match
+							
+							if (instructions[ins_ptr] == ']') {
+								rel_indent += 1;
+							} else if (instructions[ins_ptr] == '[' && rel_indent != 0) {
+								rel_indent -= 1;
+							} else if (instructions[ins_ptr] == '[' && rel_indent == 0) {
+								ins_ptr += 1; //found match, go to next command and move on
+								break;
+							}
+						}
+					} else {
+						ins_ptr += 1;
+					}
+					break;
+			}
+		}
+	}
+
+	return 0; //only goes here if it times out
 }
 
 /********************
